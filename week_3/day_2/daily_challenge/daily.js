c765d7a6788d7f9ad15a8580c7da31a245db6a67
sookaye <ashley.sookaye1@gmail.com>
2023-02-25 10:07:52 +0400

    miscs

---
+++ week_3/day_2/daily_challenge/daily.js
@@ -0,0 +1,13 @@
+const fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+fruits.splice(0,1);
+console.log(fruits);
+
+fruits.push('kiwi');
+const moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+
+console.log(first())
+moreFruits.splice(1[1],1[2]);
+moreFruits.shift();
+console.log(moreFruits);
+moreFruits.splice(0[1],1[2]);
+console.log(moreFruits.length);
